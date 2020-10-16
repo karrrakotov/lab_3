1c914ee91cd00886e306bf537042ad8ab4856902
karrrakotov <karrrakotov@mail.ru>
2020-10-16 09:19:18 +0300

    Исправил файлы

--- individual.py
+++ individual.py
@@ -1,6 +1,6 @@
 tanya_age = int(input("Введите возраст Тани: "))
 mitya_age = int(input("Введите возраст Мити: "))
-middle_age = (tanya_age + mitya_age)/2
+middle_age = (tanya_age + mitya_age) / 2
 print("Их средний возраст: " + str(middle_age))
 difference_tanya = (tanya_age - middle_age)
 difference_mitya = (mitya_age - middle_age)
